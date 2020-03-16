7fd88f7856b0e22939967e015039ddbe415214ab
何俊洁 <381548325@qq.com>
2020-03-16 16:09:45 +0800

    :hammer: 重构路由

---
+++ src/pages/wechat/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wechat">
+    wechat
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.wechat{
+  display: block;
+}
+</style>
